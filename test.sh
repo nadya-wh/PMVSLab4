c1e2ea9f057e37efc58fa70df36a6187eddecb1b
nadya-wh <nadya-wh@mail.ru>
2015-10-12 15:57:43 +0300

    Added if-else conditions to test.sh

--- test.sh
+++ test.sh
@@ -2,17 +2,37 @@ sudo insmod ./calc.ko
 sudo echo "12" > /dev/first
 sudo echo "-" > /dev/operation
 sudo echo "13" > /dev/second
-sudo cat /dev/result
+result=$(cat /dev/result)
+if [ "$result" -eq "-1" ]; then
+    echo "Test 1 passed"
+else
+    echo "Test 1 failed"
+fi
 sudo echo "12" > /dev/first
 sudo echo "+" > /dev/operation
 sudo echo "13" > /dev/second
-sudo cat /dev/result
-sudo echo "12" > /dev/first
+result=$(cat /dev/result)
+if [ "$result" -eq "25" ]; then
+    echo "Test 2 passed"
+else
+    echo "Test 2 failed"
+fi
+sudo echo "2" > /dev/first
 sudo echo "*" > /dev/operation
-sudo echo "13" > /dev/second
-sudo cat /dev/result
+sudo echo "3" > /dev/second
+result=$(cat /dev/result)
+if [ "$result" -eq "6" ]; then
+    echo "Test 3 passed"
+else
+    echo "Test 3 failed"
+fi
 sudo echo "4" > /dev/first
 sudo echo "/" > /dev/operation
 sudo echo "2" > /dev/second
-sudo cat /dev/result
+result=$(cat /dev/result)
+if [ "$result" -eq "2" ]; then
+    echo "Test 4 passed"
+else
+    echo "Test 4 failed"
+fi
 sudo rmmod calc
